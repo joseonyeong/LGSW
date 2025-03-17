9a355054e339a1988f130c441bf7c2903d5af06f
joseonyeong <whtjsdud44@gmail.com>
2025-03-17 16:16:20 +0900

    0317_의문점
    부동소수점, print함수
    출력이 원하는 데로 나오지 않아 원인 발견 및 수정

--- 0317_의문점.py
+++ 0317_의문점.py
@@ -8,3 +8,27 @@ for i in range(1,10):
     print('5','*',i,'=',x)
     x+=5
     i+=1
+
+ssss="""hello,
+        "easy" python
+    """
+print(ssss)
+
+print('소수점 이하 셋째 자리까지 부동 소수점 숫자 표시')
+a=1.0/3
+print(f'{a:.3f}')
+print('11칸 채우고 가운데 정렬')
+a='hahaha'
+print('{:^11}'.format(a))
+print(f'{a:_^11}')
+
+print('\n반복 출력')
+exstr="""
+Python : Guido van Rossum
+C++ : Bjarne Stroustrup
+Java : Jame Gosling
+Pascal : Niklaus Wirth
+    """
+print(exstr*3)
+## 왜 공백이 출력될까?
+## 코드에서 줄을 맞춘다고 들여쓰기가 자동으로 되었기 때문에 공백이 생김
