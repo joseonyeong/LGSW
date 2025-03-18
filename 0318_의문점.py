551a5b7453581e718ff155c35096884e64ccd90d
joseonyeong <whtjsdud44@gmail.com>
2025-03-18 17:11:59 +0900

    0318_의문점
    strip함수, upper함수
    반복 번수, 반쪽별

--- 0318_의문점.py
+++ 0318_의문점.py
@@ -25,39 +25,48 @@
 # else:
 #     print("비만입니다.")
 
-print("반복 번수 출력_끝내기 물어보기")
-j = 0
-k = 0
-while True:
-    num = int(input("숫자를 입력하세요: "))
-    while num > 0: #조건은 해당 코드가 돌 수 있는 조건
-        print(num)
-        num -= 1
-        k += 1
-    print(f"while문을 {k}번 반복했습니다.")
-    j += 1
+# print("반복 번수 출력_끝내기 물어보기")
+# j = 0
+# k = 0
+# while True:
+#     num = int(input("숫자를 입력하세요: "))
+#     while num > 0: #조건은 해당 코드가 돌 수 있는 조건
+#         print(num)
+#         num -= 1
+#         k += 1
+#     print(f"while문을 {k}번 반복했습니다.")
+#     j += 1
     
-    while True:
-        answer = input('끝내시겠습니까? (Y/N): ').strip().upper()
-        if answer in ['Y', 'N', 'YES', 'NO']:
-            break
-        else:
-            print('Y 또는 N으로 입력하세요.')
+#     while True:
+#         answer = input('끝내시겠습니까? (Y/N): ').strip().upper()
+#         if answer in ['Y', 'N', 'YES', 'NO']:
+#             break
+#         else:
+#             print('Y 또는 N으로 입력하세요.')
             
-    if answer == 'Y' or answer == 'YES':
-        break
-    else:
-        k = 0
-        continue
-print(f"while문을 {j}번 반복했습니다.")
-# 반복 번수 출력_끝내기 물어보기
-# 숫자를 입력하세요: 2
-# 2
-# 1
-# while문을 2번 반복했습니다.
-# 끝내시겠습니까? (Y/N): 2
-# Y 또는 N으로 입력하세요.
-# 끝내시겠습니까? (Y/N): ㅡ
-# Y 또는 N으로 입력하세요.
-# 끝내시겠습니까? (Y/N): y
-# while문을 1번 반복했습니다.
+#     if answer == 'Y' or answer == 'YES':
+#         break
+#     else:
+#         k = 0
+#         continue
+# print(f"while문을 {j}번 반복했습니다.")
+# # 반복 번수 출력_끝내기 물어보기
+# # 숫자를 입력하세요: 2
+# # 2
+# # 1
+# # while문을 2번 반복했습니다.
+# # 끝내시겠습니까? (Y/N): 2
+# # Y 또는 N으로 입력하세요.
+# # 끝내시겠습니까? (Y/N): ㅡ
+# # Y 또는 N으로 입력하세요.
+# # 끝내시겠습니까? (Y/N): y
+# # while문을 1번 반복했습니다.
+
+# print("반쪽 별")
+# for i in range(1,6):
+#     print('*'*i)
+# # *
+# # **
+# # ***
+# # ****
+# # *****
