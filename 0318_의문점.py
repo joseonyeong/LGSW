fca0ca2cbb3908e4745eee39589f9b38d80307c9
joseonyeong <whtjsdud44@gmail.com>
2025-03-18 11:10:52 +0900

    BMI 계산기
    사용자가 숫자만 입력할 수 있도록 예외처리해봤다.

---
+++ 0318_의문점.py
@@ -0,0 +1,27 @@
+# print("BMI 계산기_등급출력_숫자만 입력받을 수 있게")
+# while True:
+#     try:
+#         weight = float(input("몸무게 입력: "))
+#         break
+#     except ValueError:
+#         print("숫자로만 입력하세요.")
+
+# while True:
+#     try:
+#         height = float(input("키 입력: "))
+#         break
+#     except ValueError:
+#         print("숫자로만 입력하세요.")
+
+# bmi = weight / height**2
+# print(f'BMI: {bmi:.3f}')
+
+# if bmi < 18.5:
+#     print("저체중입니다.")
+# elif bmi < 23:
+#     print("정상입니다.")
+# elif bmi < 25:
+#     print("과체중입니다.")
+# else:
+#     print("비만입니다.")
+
